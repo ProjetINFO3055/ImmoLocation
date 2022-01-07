829be37a77bee71b9e29685682a754691db87e6f
pathi14 <pathipayong@gmail.com>
2022-01-07 11:32:00 +0100

    creation controller propriété

---
+++ src/controleur/ProprrieteBdController.java
@@ -0,0 +1,12 @@
+package controleur;
+
+import java.sql.Connection;
+
+public class ProprrieteBdController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/controller/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author PAVONE
- *
- */
-package controller;
