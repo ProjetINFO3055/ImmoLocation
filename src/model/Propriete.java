a117325c39c66fe0a8e8849e136763e58ed11a7c
ftsjordan <ftsjordan98@gmail.com>
2022-01-16 01:04:46 +0100

    debut de l'elaboration des proprietes

--- src/model/Propriete.java
+++ src/model/Propriete.java
@@ -1,15 +1,58 @@
 package model;
 
-public class Propriete {
+public abstract class Propriete {
 	
-	
-	protected int type_propriete;
-	protected int prix_mensuel;
-	protected String description;
-	protected String localisation;
-	protected int status;
+	private String type;
+	private float prix;
+	private String description;
+	private String localisation;
+	private boolean statut;
 
+	public Propriete() {}
 	
+	public Propriete(String type, float prix, String description, String localisation, boolean statut) {
+		
+		this.type=type;
+		this.prix=prix;
+		this.description=description;
+		this.localisation=localisation;
+		this.statut=statut;	
 	
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public float getPrix() {
+		return prix;
+	}
+
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+
+	public abstract String getDescription();
+
+
+	public String getLocalisation() {
+		return localisation;
+	}
+
+	public void setLocalisation(String localisation) {
+		this.localisation = localisation;
+	}
+
+	public boolean isStatut() {
+		return statut;
+	}
+
+	public void setStatut(boolean statut) {
+		this.statut = statut;
+	}
+
 }
-	
