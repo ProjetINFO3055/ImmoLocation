3a4678bb8c3fe1062f3bf4edc3aec4600050cef3
Feujio Tsagmo <52106771+ftsjordan@users.noreply.github.com>
2022-01-19 02:52:51 +0100

    traitement du controlleur location en cour

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ import java.awt.EventQueue;
 import src.controleur.*;
 import java.sql.*;
 import java.util.ArrayList;
+
+import controleur.LocationController;
+import model.Locataire;
+import model.Maison;
+import model.Propriete;
 import src.model.*;
 import src.vue.Application;
 import src.vue.LoginFrame;
@@ -25,7 +30,28 @@ public class Main {
 		LoginFrame log=new LoginFrame();
 		log.lanceur();
 					 
-	
+	/////////////////////////////////////////Test en console/////////////////////////////////::
+		
+		/*ConnexionBD conn=new ConnexionBD();
+		String requete="SELECT * FROM propriete";
+		ResultSet res=conn.select(requete);
+		ProprieteBdController a=new ProprieteBdController();
+		ArrayList b=new ArrayList();
+		b=a.afficherToutesLesProprietes(res);
+		Propriete p=new Maison();
+		Propriete x=new Boutique();
+		for(int i=0;i<b.size();i++) {
+			System.out.println(i+" "+b.get(i));
+		}
+		
+		Propriete p=new  Maison(5,100000,"djqlhqjk","lsdjeozalisation dans ke dile",2);
+		Locataire l=new  Locataire(17413," eloko", "Stephanie", 655543452, "enseignante");
+		LocationController louer=new LocationController();
+		louer.assigner(l, p);
+		System.out.println("le id est maintenant a : "+p.getId());
+		*/
+		
+		
 						
 		
 		
