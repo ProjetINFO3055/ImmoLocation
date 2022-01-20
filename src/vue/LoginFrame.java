3cdd57bed939590468146ff6833db3bcbf20646a
feumoeouvaneauxilia <feumoeouvaneauxilia@gmail.com>
2022-01-20 13:26:27 +0100

    gestion de conflit

--- src/vue/LoginFrame.java
+++ src/vue/LoginFrame.java
@@ -8,13 +8,13 @@ import java.sql.ResultSet;
 
 import javax.swing.JOptionPane;
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 import com.mysql.cj.xdevapi.Result;
 
 import src.ConnexionBD;
-=======
+//=======
 
->>>>>>> 37b4fe7d690d186af95fbd9bced3b8ef439af891
+//>>>>>>> 37b4fe7d690d186af95fbd9bced3b8ef439af891
 import src.controleur.AthentificationBdController;
 import src.model.Authentification;
 
