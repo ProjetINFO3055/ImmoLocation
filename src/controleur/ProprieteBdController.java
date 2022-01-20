164dcb5ee8a5704cdc346abb43eb5fc54f3366eb
feumoeouvaneauxilia <feumoeouvaneauxilia@gmail.com>
2022-01-20 09:07:31 +0100

    ProprieteBdcontroller

--- src/controleur/ProprieteBdController.java
+++ src/controleur/ProprieteBdController.java
@@ -1,6 +1,18 @@
-package controleur;
+package src.controleur;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import src.ConnexionBD;
+import src.model.*;
 
 public class ProprieteBdController {
+static ConnexionBD conn = new ConnexionBD();
+	
+	
+
 	
 
 		
